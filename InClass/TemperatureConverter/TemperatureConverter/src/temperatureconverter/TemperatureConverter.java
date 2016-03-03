eee1f5ed85e931d90927c4242902541bcaeadd58
Mauricio S. Perez <Maurime2@gmail.com>
2016-03-03 14:43:28 -0800

    Signed-off-by: Mauricio S. Perez <Maurime2@gmail.com>

---
+++ InClass/TemperatureConverter/TemperatureConverter/src/temperatureconverter/TemperatureConverter.java
@@ -0,0 +1,9 @@
+package temperatureconverter;
+
+public class TemperatureConverter {
+
+    public static void main(String[] args) {
+        FToCFrame myForm = new FToCFrame();        
+    }
+    
+}
