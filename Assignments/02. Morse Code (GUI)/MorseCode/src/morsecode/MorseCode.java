381730e1af57fdf5068286506b5825880e434214
Mauricio S. Perez <maurime2@gmail.com>
2016-03-25 11:39:57 -0700

    Signed-off-by: Mauricio S. Perez <maurime2@gmail.com>

---
+++ Assignments/02. Morse Code (GUI)/MorseCode/src/morsecode/MorseCode.java
@@ -0,0 +1,12 @@
+//Libraries
+package morsecode;
+
+//Class MorseCode
+public class MorseCode {
+
+    //Main Start
+    public static void main(String[] args) {
+        
+    }//Main End
+    
+}//Class MorseCode End
