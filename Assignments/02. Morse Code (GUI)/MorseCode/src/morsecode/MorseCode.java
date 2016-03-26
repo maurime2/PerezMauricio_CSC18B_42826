b31eb815178eb2601cfaa2442dd93ea7d60c7056
Mauricio S. Perez <maurime2@gmail.com>
2016-03-25 18:32:42 -0700

    Signed-off-by: Mauricio S. Perez <maurime2@gmail.com>

--- Assignments/02. Morse Code (GUI)/MorseCode/src/morsecode/MorseCode.java
+++ Assignments/02. Morse Code (GUI)/MorseCode/src/morsecode/MorseCode.java
@@ -10,9 +10,9 @@ public class MorseCode {
         Scanner input = new Scanner (System.in);    //creates a scanner "input"
         Boolean loop = false;     //For Selection Loop 
         Boolean morse = false;   //False = Ascii to Morse, True = Morse to Ascii
-        String Input;           //Will Hold Inputs Temporarily
-        String Original;       //Will Hold Original String
-        String Converted;     //Will Hold Converted String
+        String Input = "";           //Will Hold Inputs Temporarily
+        String Original = "";       //Will Hold Original String
+        String Converted = "";     //Will Hold Converted String
         //Choose Morse or Ascii Input
         do{
         System.out.print("Will You Be Inputing Characters(1) or Morse(2)? ");
@@ -20,42 +20,50 @@ public class MorseCode {
         if(Input.charAt(0)== '1' || Input.charAt(0)== '2'){
             //Input is true - Morse to Ascii
             if(Input.charAt(0)== '2'){
-                morse = true;
+                morse = true;   //Morse to Ascii
             }
             else{
-                morse = false;
+                morse = false; //Ascii to Morse
             }
             loop = true;
         }//End Choose Morse or Ascii Input
         
         }while(loop == false);
+        
         System.out.println("Your Input is = "+Input);
+        
     
         //Input Ascii to Morse
         if(morse==false){
-        System.out.print("\n\nAscii to Morse\n Please input your message to convert to Morse Code: ");
-        Input=input.nextLine();
-        System.out.println("Your Input is="+Input);
+        System.out.print("\nAscii to Morse\n Please input your message to convert to Morse Code: ");
+        Original=input.nextLine();
+        System.out.println("Your Input is="+Original);
         }
         
         //Input Morse to Ascii
         else if(morse==true){
-        System.out.print("\n\nMorse to Ascii\n Please input your message to convert to Morse Code: ");
-        Input=input.nextLine();
-        System.out.println("Your Input is="+Input);
+        System.out.print("\nMorse to Ascii\n Please input your message to convert to Morse Code: ");
+        Original=input.nextLine();
+        System.out.println("Your Input is="+Original);
         }
         
         //Clean Up
+        System.out.print("\nClean Up\n");
+        
             //Ascii to Morse Clean Up
             if(morse==false){
-            System.out.print("A-Z,a-z,0-9, and spaces:\n");
+            System.out.print("Allow Message to only accept a-z,0-9.");
+               Converted = Original.toLowerCase();//Converts All to Lowercase.
+               Converted = Converted.replaceAll("[^0-9a-z ]","");//-Punctuation.
             }
 
             //Morse to Ascii clean up
             else if(morse==true){
-            System.out.print("Morse Code only:\n");
+            System.out.print("Allow Message to only accept Morse Code.");
+               Converted = Original.replaceAll("[^ ^.^-]","");//-Punctuation.
             }
-        System.out.println("\n\nYour input cleaned up:\n"+Input);
+        //Clean String to Convert
+        System.out.println("\nYour new cleaned up String is: "+Converted);
         
     }//Main End
     
