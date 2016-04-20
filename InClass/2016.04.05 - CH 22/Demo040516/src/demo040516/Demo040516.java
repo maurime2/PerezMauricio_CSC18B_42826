3482f3ba13d863b46c6dabf6c634641c6f9e0ead
Mauricio S. Perez <maurime2@gmail.com>
2016-04-19 18:14:09 -0700

    Signed-off-by: Mauricio S. Perez <maurime2@gmail.com>

---
+++ InClass/2016.04.05 - CH 22/Demo040516/src/demo040516/Demo040516.java
@@ -0,0 +1,12 @@
+//Libraries
+package demo040516;
+
+//Class Demo040516
+public class Demo040516 {
+
+    //Main Start
+    public static void main(String[] args) {
+        new MainFrame().setVisible(true);
+    }//End Main
+    
+}//End Class Demo040516
